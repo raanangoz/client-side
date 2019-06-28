72f145f6583872a42aa359814b8155eec011b2da
itzikd1 <vhhchh20>
2019-06-28 23:18:19 +0300

    updatedafter2weeks

--- ClientSide/Register/RegisterController.js
+++ ClientSide/Register/RegisterController.js
@@ -1,6 +1,22 @@
 // interestPoints controller
 angular.module("myApp")
     .controller("RegisterController", function ($scope,$http,$rootScope) {
+        $scope.showQuestions = function (){
+            var req = {
+                method: 'GET',
+                url: 'http://localhost:3000/get_validation_questions'
+            };
+            $http (req).then(function mySuccess(response) {
+                $scope.q0 = response.data[0];
+                $scope.q1 = response.data[1];
+                $scope.q2 = response.data[2];
+                $scope.q3 = response.data[3];
+            }, function myError(response) {
+                $scope.result = response.data;
+
+            })
+        }
+
         $scope.register = function () {
             // var _username = $scope.username;
             // var _password = $scope.password;
@@ -18,7 +34,9 @@ angular.module("myApp")
                     country: $scope.country,
                     email: $scope.email,
                     password: $scope.password,
-                    interests: ["Culture","Food"]//TODO not really
+                    interests: ["Culture","Food"],//TODO not really
+                    questions: [$scope.q0,$scope.q1,$scope.q2,$scope.q3],//TODO
+                    answers: [$scope.a0,$scope.a1,$scope.a2,$scope.a3]//TODO
                 }
 
             };
@@ -26,6 +44,7 @@ angular.module("myApp")
                 .then(function mySuccess(response) {
                     $scope.result = response.data;
                 }, function myError(response) {
+                    console.log(response.data.statusText);
                     $scope.result = response.data;
 
                 })
